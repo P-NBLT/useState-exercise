a72fa731ff3fc073d753665603bf852d76c47bee
Pierre-Alexis Noblet <98712114+P-NBLT@users.noreply.github.com>
2022-08-30 17:10:21 +0200

    upt

---
+++ component/scoreboard/Scoreboard.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const Scoreboard = (props) => {
+  const [score, setScore] = useState([0, 0, 0]);
+  const names = ["Rein", "Maylis", "Gerrit"];
+
+  const decrement = (e) => {
+    const copy = [...score];
+    copy[e.target.id]--;
+    return setScore(copy);
+  };
+  const increment = (e) => {
+    const copy = [...score];
+    copy[e.target.id]++;
+    return setScore(copy);
+  };
+
+  return (
+    <div className="scoreboard">
+      {names.map((el, idx) => (
+        <div
+          key={idx}
+          style={{
+            display: "flex",
+            rowGap: "40px",
+            columnGap: "20px",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "green",
+            margin: "20px 0 20px 0",
+            width: "200px",
+          }}
+        >
+          <p>{el}</p>
+          <button
+            style={{ width: "20px", height: "20px" }}
+            onClick={decrement}
+            id={idx}
+          >
+            -
+          </button>
+          <p>{score[idx]}</p>
+          <button
+            style={{ width: "20px", height: "20px" }}
+            onClick={increment}
+            id={idx}
+          >
+            +
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+Scoreboard.propTypes = {};
+
+export default Scoreboard;
